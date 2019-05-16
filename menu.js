cdac29bd92db67cfc2f746de77938d6693204389
Renkima <39596570+Renkima@users.noreply.github.com>
2019-05-16 12:12:04 +0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,15 @@
+/*_______________________________________________*/
+
+
+
+
+
+
+
+/*_________________ для кнопки с градиентом ______________________________*/
+document.querySelector('.butty').onmousemove = function (e) {
+var x = e.pageX - e.target.offsetLeft;
+var y = e.pageY - e.target.offsetTop;
+e.target.style.setProperty('--x', x + 'px');
+e.target.style.setProperty('--y', y + 'px');
+};
